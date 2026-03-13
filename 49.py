6d06036244611d1c742d8e4e65db470da317c422
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-03-14 02:02:46 +0530

    solved

---
+++ 49.py
@@ -0,0 +1,77 @@
+import os,sys
+from io import BytesIO,IOBase
+from bisect import bisect_left,bisect_right
+import math
+from itertools import permutations
+
+
+#Copied from https://codeforces.com/blog/entry/71884
+BUFSIZ=8192
+class FastIO(IOBase):
+    newlines=0
+    def __init__(self,file):
+        self._fd=file.fileno()
+        self.buffer=BytesIO()
+        self.writable="n"in file.mode or "r" not in file.mode
+        self.write=self.buffer.write if self.writable else None
+    def read(self):
+        while True:
+            b=os.read(self._fd,max(os.fstat(self._fd).st_size,BUFSIZ))
+            if not b:
+                break
+            ptr=self.buffer.tell()
+            self.buffer.seek(0,2),self.buffer.write(b),self.buffer.seek(ptr)
+        self.newlines=0
+        return self.buffer.read()
+    def readline(self):
+        while self.newlines==0:
+            b=os.read(self._fd,max(os.fstat(self._fd).st_size, BUFSIZ))
+            self.newlines=b.count(b"\n")+(not b)
+            ptr=self.buffer.tell()
+            self.buffer.seek(0, 2),self.buffer.write(b),self.buffer.seek(ptr)
+        self.newlines-=1
+        return self.buffer.readline()
+    def flush(self):
+        if self.writable:
+            os.write(self._fd,self.buffer.getvalue())
+            self.buffer.truncate(0),self.buffer.seek(0)
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer=FastIO(file)
+        self.flush=self.buffer.flush
+        self.writable=self.buffer.writable
+        self.write=lambda s:self.buffer.write(s.encode("ascii"))
+        self.read=lambda:self.buffer.read().decode("ascii")
+        self.readline=lambda:self.buffer.readline().decode("ascii")
+
+#if os.path.exists("input.txt"):
+#    sys.stdin=open("input.txt", "r")
+#    sys.stdout=open("output.txt", "w")
+# sys.stdin,sys.stdout = IOWrapper(sys.stdin),IOWrapper(sys.stdout)
+if sys.version_info[0]<3:
+    sys.stdin,sys.stdout=FastIO(sys.stdin),FastIO(sys.stdout)
+else:
+    sys.stdin,sys.stdout=IOWrapper(sys.stdin),IOWrapper(sys.stdout)
+input=lambda:sys.stdin.readline().rstrip("\r\n")
+
+#bootstarp-pyboot
+
+def prime(x):
+    if x==0 or x==1:
+        return False
+    if x==2 or x==3:
+        return True
+    if x%2==0 or x%3==0:
+        return False
+    for i in range(5,int(x**0.5),6):
+        if x%i==0 or x%(i+2)==0:
+            return False
+    return True
+
+for i in range(1000,6671):
+    x=i
+    y=x+3330
+    z=y+3330
+    if prime(x) and prime(y) and prime(z) and sorted(str(x))==sorted(str(y)) and sorted(str(y))==sorted(str(z)):
+        print(x,y,z)
